4a07edfdf282d74048dccf12b203e6dfd8456843
Justin Gonzales <jgonza1427@gmail.com>
2022-05-24 19:56:02 -0700

    Tkinter integration to send Tweet

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import tweepy
-import tkinter
+import tkinter as tk
 from datetime import datetime, date, time, timezone
 import os
 from dotenv import load_dotenv
+import requests
+from ctypes import windll
 
 load_dotenv()
 
@@ -16,8 +18,49 @@ auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 
-if __name__ == '__main__':
-    current_time = datetime.now().time()
-    print(current_time)
+def hello():
+    print("helloWorld")
 
-    api.update_status("Posting now, at " + str(current_time))
+
+def tK_init():
+    root = tk.Tk()
+    root.geometry('500x500')
+    root.title('Tweepy Bot')
+
+    message = tk.Label(root, text="Welcome to Tweepy Bot. Please press the button to send a random tweet.")
+    message.pack()
+
+    button = tk.Button(root, text="Send Tweet", command=tweet_quote)
+    button.pack(ipady=50, ipadx=50, expand=True)
+
+    try:
+        from ctypes import windll
+
+        windll.shcore.SetProcessDpiAwareness(1)
+    finally:
+        root.mainloop()
+
+def get_random_quote():
+    len_response = 1000
+    quoteText = ''
+    while len_response > 280:
+        response = requests.get("https://quote-garden.herokuapp.com/api/v3/quotes/random")
+        if response.status_code == 200:
+            json_data = response.json()
+            data = json_data['data']
+            quoteText = str(data[0]['quoteText'])
+            len_response = len(quoteText)
+    return quoteText
+
+
+def tweet_quote():
+    quote = get_random_quote()
+    tweet = '"' + quote + '"'
+    api.update_status(tweet)
+    print("Tweet sent successfully.")
+
+if __name__ == '__main__':
+
+    tK_init()
+
+
